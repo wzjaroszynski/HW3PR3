aa248938765444f62df6da239c58fd92dad7a319
wzjaroszynski <112191003+wzjaroszynski@users.noreply.github.com>
2022-09-12 03:17:45 +0000

    final

--- .ccls-cache/@home@runner@HW3PR3/main.cpp
+++ .ccls-cache/@home@runner@HW3PR3/main.cpp
@@ -9,8 +9,15 @@ int main () {
   
     double E, R, RC, t, i, e_pow;
     double e;
+     e = 2.71828;
+    E = 20;
+    R = 10;
+    RC = 0.044;
+    t =0.023;
   
+    e_pow = pow(e,(-t/RC));
+    i = ((E/R)*e_pow);
+  
+    cout << i;
  
-
-  
 }
